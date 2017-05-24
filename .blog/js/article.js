458c3db0c67d28d6a245d8ad2bada45d83559b78
fenpan.lfp <linfenpan@corp.netease.com>
2017-05-24 14:57:51 +0800

    删除评论板块

--- .blog/js/article.js
+++ .blog/js/article.js
@@ -1,6 +1,1 @@
-// module: 添加友言评论插件
-$(function(){
-    $.getScript("http://v2.uyan.cc/code/uyan.js?uid=2072885", function(){
-        console.log("友言安装成功，使用的是当前邮箱帐号");
-    });
-});
+// 评论板块移除了，等找到更好的替代品再说
